86ed640a4ec1e72eb6411f860888ebfa5551fbe0
Layne Britton <laynebritton1998@gmail.com>
2023-07-14 15:44:38 -0400

    Update to minecraft server 1.4 beta stable (#24)
    * [#22] Update minecraft server to 1.4 beta-stable. Bump behavior pack version
    
    * Remove old 0 param from setblock command
    
    * Fix manifest @minecraft/server version

--- scripts/Utilities.ts
+++ scripts/Utilities.ts
@@ -1,7 +1,7 @@
 import { Vector3, Dimension, MinecraftBlockTypes } from "@minecraft/server";
-
+import { say } from "./Debug";
 export const destroy = (blockLocation: Vector3, dimension: Dimension) => {
   dimension.runCommandAsync(
-    `setblock ${blockLocation.x} ${blockLocation.y} ${blockLocation.z} ${MinecraftBlockTypes.air.id} 0 destroy`
+    `setblock ${blockLocation.x} ${blockLocation.y} ${blockLocation.z} ${MinecraftBlockTypes.air.id} destroy`
   );
 };
