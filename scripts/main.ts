2b3870be9fdb15bd13b8a13188bc323b826ff13c
jessejohn01 <jesse.arstein@yahoo.com>
2023-09-21 12:00:30 -0600

    Moved new required dependancies around.

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,5 +1,4 @@
 import { world, PlayerBreakBlockAfterEvent } from "@minecraft/server";
-import {} from "@minecraft/vanilla-data";
 import { VeinBreakEvent } from "./VeinBreakEvent";
 
 world.afterEvents.playerBreakBlock.subscribe(
